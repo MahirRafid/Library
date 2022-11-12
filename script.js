b87d55598e9560e7f21494a812695aa371f1f8c8
Mahir Mosleh <mahirmosleh136@gmail.com>
2022-11-12 12:54:03 +0600

    Added html and css for the topBar, copyright and main section part

---
+++ script.js
@@ -0,0 +1,4 @@
+let copyRight = document.querySelector(".copyright");
+let currentYear = new Date().getFullYear();
+// console.log(currentYear);
+copyRight.innerHTML = `<p>Copyright&nbsp;&nbsp;©&nbsp;&nbsp;Mahir Mosleh&nbsp;&nbsp;${currentYear}</p>`;
